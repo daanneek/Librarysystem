0af2a05b72b201c05f1b87b749a2368501b08608
daanneek <daanneek@gmail.com>
2019-05-28 20:57:13 +0200

    first improvement PLS
    Signed-off-by: daanneek <daanneek@gmail.com>

--- ACTUALPLS.py
+++ ACTUALPLS.py
@@ -1,118 +1,186 @@
 import json
 import csv
 
-
 class Library:
 
-    def __init__(this):
-        this.book_list = []
-        this.customer_list = []
-        this.book_loans = []
+    def __init__(self):
+        self.book_list = []
+        self.customer_list = []
+        self.book_loans = []
 
-    def import_data(this):
+
+    def import_data(self):
         """Takes a csv and a json file and outputs their contents into lists"""
-        with open('FakeNameSet.csv', 'rt', encoding="utf-8-sig", newline='') as f:
+        with open("FakeNameSet.csv", 'rt', encoding="utf-8-sig", newline='') as f:
             csv_reader = csv.reader(f)
             row1 = next(csv_reader)
             del row1
             for line in csv_reader:
-                this.customer_list.append(line)
+                self.customer_list.append(line)
 
-        with open('booksset1.json') as json_file:
-            this.book_list = json.load(json_file)
+        with open("booksset1.json") as json_file:
+            self.book_list = json.load(json_file)
 
-    def display_customer_list(this):
-        print(this.customer_list)
+    def get_customer_list(self):
+        """Returns all information in the customer list."""
+        print(self.customer_list)
+
+    def get_book_list(self):
+        """Returns all information in the book list"""
+        print(self.book_list)
+
+    def get_loans_list(self):
+        """Returns all information in the loans list"""
+        print(self.book_loans)
+
+    def get_book_by(this, something):
+        """Search for a book by name, author, or country of origin, returns all available information about the book."""
+        for books in this.book_list:
+            if books['title'] == something or books['author'] == something or books['country'] == something:
+                print(books)
 
     def get_book_by_title(this, title):
         """Takes a book's title and returns all available information about the book."""
-        for book in  this.book_list:
+        for book in this.book_list:
             if book['title'] == title:
-                print (book)
+                print(book)
 
-    def get_book_by_author(this, author):
+    def get_book_by_author(self, author):
         """Takes an author's name as input and returns all available information about the book."""
-        for book in  this._book_list:
+        for book in self.book_list:
             if book['author'] == author:
-                print (book)
+                print(book)
 
-    def check(this, title):
+    def add_book_to_library(self, new_book):
+        """Takes the necessary information for a book and adds it to the book_list, not to the backup json file."""
+        self.book_list.append(new_book)
+
+    def add_customer(self, new_customer):
+        """A class that adds a customer to the library's register. This does NOT add the customer to the csv file."""
+        Library.customer_list.append(new_customer)
+
+    def check_book(self, title):
         """Takes the title of a book and checks if it exists in the book list."""
-        for books in this.book_list:
+        for books in self.book_list:
             if books['title'] == title:
                 return True
 
-    def backup_library(self,library):
-        with open('bookssetbackup.json', 'w') as outfile:
-            json.dump(library, outfile)
-
-    def backup_customers(self, customerlist):
-        toCSV = customerlist
-        with open("FakeNameSetbackUp.csv", "w", encoding="utf-8-sig") as f:
-            writer = csv.writer(f)
-            writer.writerows(toCSV)
-
-    def backup_loans(self, customerlist):
-        with open('loansbackup.json', 'w') as outfile:
-            json.dump(customerlist, outfile)
-
-    def check_person(this, name):
+    def check_person(self, name):
         """Takes the name of a customer and checks if the person exists in the customer list."""
-        if name in [j for i in this.customer_list for j in i]:
+        if name in [j for i in self.customer_list for j in i]:
             return True
 
-    def check_bookloans(this, title):
+    def check_bookloans(self, title):
         """Takes the title of a book and checks if it exists in the book_loans list."""
-        if any(title in sl for sl in this.book_loans):
+        if any(title in books for books in self.book_loans):
             return True
         else:
             return False
 
-    def add_book_to_library(this, new_book):
-        """Takes the necessary information for a book and adds it to the book_list, not to the backup json file."""
-        this.book_list.append(new_book)
-
-    def add_customer(self, new_customer):
-        """A class that adds a customer to the library's register. NOTE this does NOT add the customer to the csv file."""
-        lib1.customer_list.append(new_customer)
-
-    def loan(this, name, title):
+    def loan(self, name, title):
         """Loans a book to a person and checks if the person and book exists, weather the book is already loaned out."""
-        if lib1.check(title) and lib1.check_person(name) and not this.book_loans:
+        if self.check_book(title) and self.check_person(name) and not self.book_loans:
             book = str(title)
             person = str(name)
             loan = [person, book]
-            this.book_loans.append(loan)
+            self.book_loans.append(loan)
         else:
-            if lib1.check(title) and lib1.check_person(name) and not lib1.check_bookloans(title):
+            if Library.check_book(title) and Library.check_person(name) and not Library.check_bookloans(title):
                 book = str(title)
                 person = str(name)
                 loan = [person, book]
-                this.book_loans.append(loan)
+                self.book_loans.append(loan)
 
+
+class Catalog(Library):
+    @staticmethod
+    def get_all_books():
+        """A method that displays ALL book titles available in the library, could be loaned out."""
+        for book in Library.book_list:
+            print(book['title'])
+
+
+class Authors():
+    @staticmethod
+    def get_all_authors_books(library):
+        """A method that displays ALL authors and their books available in the library, without duplicates/unknown."""
+        author_list = []
+        for author in library.book_list:
+            if author['author'] != 'Unknown':
+                author_list.append((author['author'], author['title']))
+        print(list(dict(dict.fromkeys(author_list))))
+
+
+class Administration(Library):
+    """Class administration goes over the saving of the parts of the library system."""
+    @staticmethod
+    def backup_library(library):
+        with open('bookssetbackup.json', 'w') as outfile:
+            json.dump(library, outfile)
+
+    @staticmethod
+    def backup_customers(customerlist):
+        toCSV = customerlist
+        with open("FakeNameSetbackUp.csv", "w", encoding="utf-8-sig", newline ='') as f:
+            writer = csv.writer(f)
+            writer.writerows(toCSV)
+
+    @staticmethod
+    def load_customers_from_backup():
+        """takes a csv backup file and loads it into customer list."""
+        with open('FakeNameSetbackup.csv', 'rt', encoding="utf-8-sig", newline='') as f:
+            csv_reader = csv.reader(f)
+            for line in csv_reader:
+                Library.customer_list.append(line)
+
+    @staticmethod
+    def load_books_from_backup():
+        """takes a json backup file and loads it into the book list"""
+        with open("bookssetbackup.json") as json_file:
+            Library.book_list = json.load(json_file)
+
+
+class loanAdministration:
+    @staticmethod
+    def backup_loans(customerlist):
+        with open('loansbackup.json', 'w') as outfile:
+            json.dump(customerlist, outfile)
+
+    @staticmethod
+    def load_loans_from_backup():
+        """Takes a json backup file and loads it into the book loans list"""
+        with open("loansbackup.json") as json_file:
+            Library.book_loans = json.load(json_file)
 
 if __name__ == "__main__":
-    lib1 = Library()
-    lib1.import_data()
 
-    lib1.loan("Hisham", 'Pride and Prejudice')
-    lib1.loan("Hisham", 'Things Fall Apart')
-    lib1.loan("Rodin", 'The Sound of the Mountain')            #if a person does not exist the loan does not get added,
-    lib1.loan("Maren", 'Berlin Alexanderplatz')                #if a book gets added that doesn't exist neither.
-    lib1.loan("Rodin", 'Berlin Alexanderplatz')                #if a book that is already loaned out gets loaned again,
-    lib1.loan("Hisham", "REEEEEE")                             #it does not show up in the bookloans list.
-    #print(lib1.book_loans)
+    # Initializing the Library
+    Library = Library()
+    Library.import_data()
+    #Administration.load_customers_from_backup()            # Add this line to read from a backed-up customer list.
+    #Administration.load_books_from_backup()                # Add this line to read from a backed-up book list.
+    #Administration.load_loans_from_backup()                # Add this line to read from a backed-up loans list.
 
-    lib1.check_bookloans('Berlin Alexanderplatz')
-    lib1.check_bookloans("REEEEEE")
+    # Adding loans to a loaned books list, if a person does not exist the loan does not get added, if a book gets added
+    # that doesn't exist neither. If a book that is loaned out gets loaned, it does not show up in the book_loans list.
+    #Authors.get_all_authors_books(Library)                 # Puts every author with their respective book in a list, "unknown" authors excluded
+    Library.loan("Hisham", 'Pride and Prejudice')
+    Library.loan("Hisham", 'Things Fall Apart')
+    Library.loan("Rodin", 'The Sound of the Mountain')
+    Library.loan("Rodin", 'Berlin Alexanderplatz')
+    Library.loan("Hisham", "REEEEEE")
+    Library.loan("Maren", 'Sentimental Education')
+    #Library.get_book_by("Jane Austen")
+    #Library.get_book_by("Berlin Alexanderplatz")   # prints available information for a book
+    #Library.get_customer_list()                    # prints all customers in the customer administration
+    #Catalog.get_all_books()                        # Prints all book titles in the Library
 
     #lib1.get_book_by_title("The Sound of the Mountain")
-    lib1.add_book_to_library({"author": "TstAuthor","country": "TestCountry","imageLink": "images/TestImage","language": "TestLanguage","link": "Testlink\n","pages": 1,"title": "No","year": 1836})
-    lib1.backup_library(lib1.book_list)
-    lib1.add_customer(['21',  'male',  'Dutch',  'Daan',  'Nekeman',  'Straat 1',  '9461 JE',  'Gieten',  'GulserenWilligenburg@teleworm.us',  'Ressoare',  '06-92433659'])
-    lib1.backup_customers(lib1.customer_list)
-    lib1.backup_loans(lib1.book_loans)
-
+    Library.add_book_to_library({"author": "TstAuthor", "country": "TestCountry", "imageLink": "images/TestImage", "language": "TestLanguage", "link": "Testlink\n", "pages": 1, "title": "No", "year": 1836})
+    Administration.backup_library(Library.book_list)
+    Library.add_customer(['21', 'male', 'Dutch', 'Daan', 'Nekeman', 'Straat 1', '9461 JE', 'Gieten', 'GulserenWilligenburg@teleworm.us', 'Ressoare', '06-92433659'])
+    Administration.backup_customers(Library.customer_list)
+    loanAdministration.backup_loans(Library.book_loans)
 
 
 
--- OLDPLS.py
+++ OLDPLS.py
@@ -1,110 +1,121 @@
 import json
 import csv
-from collections import OrderedDict
-import pprint
+
 
 class Library:
-    def __init__(self, book_list):
-        self._book_list = book_list
 
+    def __init__(self):
+        self.book_list = []
+        self.customer_list = []
+        self.book_loans = []
+
+    def import_data(self):
+        """Takes a csv and a json file and outputs their contents into lists"""
+        with open('FakeNameSet.csv', 'rt', encoding="utf-8-sig", newline='') as f:
+            csv_reader = csv.reader(f)
+            row1 = next(csv_reader)
+            del row1
+            for line in csv_reader:
+                self.customer_list.append(line)
+
+        with open('booksset1.json') as json_file:
+            self.book_list = json.load(json_file)
+
+
+    def display_customer_list(self):
+        print(self.customer_list)
 
     def get_book_by_title(self, title):
-        for book in  self._book_list:
+        """Takes a book's title and returns all available information about the book."""
+        for book in self.book_list:
             if book['title'] == title:
-                print (book)
-
+                print(book)
 
     def get_book_by_author(self, author):
-        for book in  self._book_list:
+        """Takes an author's name as input and returns all available information about the book."""
+        for book in self.book_list:
             if book['author'] == author:
-                print (book)
+                print(book)
 
+    def check(self, title):
+        """Takes the title of a book and checks if it exists in the book list."""
+        for books in self.book_list:
+            if books['title'] == title:
+                return True
 
-    def get_all_authors(self):
-        authors = [x["author"] for x in self._book_list]
-        print(authors)
-
-
-    def add_book_to_library(self, new_book):
-        self._book_list.append(new_book)
-
-    def move_rented_book(self,title):
-        for book in range(len(booklist)-1):
-            if booklist[book]['title'] == title:
-                del booklist[book]
-
-    def backup_library(self,library):
+    def backup_library(self, library):
         with open('bookssetbackup.json', 'w') as outfile:
             json.dump(library, outfile)
 
-
-class CustomerAdministration(Library):
-    def __init__(self):
-        self.customer_list = []
-
-    def add_customer(self, new_customer):
-        """A class that adds a customer to the library's register. NOTE this does NOT add the customer to the csv file."""
-        customer_list.append(new_customer)
-
-    @staticmethod
-    def backup_customers():
-        toCSV = customer_list
+    def backup_customers(self, customerlist):
+        toCSV = customerlist
         with open("FakeNameSetbackUp.csv", "w", encoding="utf-8-sig") as f:
             writer = csv.writer(f)
             writer.writerows(toCSV)
 
+    def backup_loans(self, customerlist):
+        with open('loansbackup.json', 'w') as outfile:
+            json.dump(customerlist, outfile)
 
+    def check_person(self, name):
+        """Takes the name of a customer and checks if the person exists in the customer list."""
+        if name in [j for i in self.customer_list for j in i]:
+            return True
 
+    def check_bookloans(self, title):
+        """Takes the title of a book and checks if it exists in the book_loans list."""
+        if any(title in sl for sl in self.book_loans):
+            return True
+        else:
+            return False
 
-class loanbooks(CustomerAdministration, Library):
-    def __init(self):
-        self.bookloans = []
+    def add_book_to_library(self, new_book):
+        """Takes the necessary information for a book and adds it to the book_list, not to the backup json file."""
+        self.book_list.append(new_book)
 
-    def rent_book(person, title):
-        bookloans = []
+    def add_customer(self, new_customer):
+        """A class that adds a customer to the library's register. NOTE this does NOT add the customer to the csv file."""
+        lib1.customer_list.append(new_customer)
 
-        if person in customer_list:
-            if title in booklist['title']:
-                x = str(person)
-                y = str(title)
-                a = {x: y}
-                bookloans.append(dict(a))
-                print("SUCCESS")
+    def loan(self, name, title):
+        """Loans a book to a person and checks if the person and book exists, weather the book is already loaned out."""
+        if lib1.check(title) and lib1.check_person(name) and not self.book_loans:
+            book = str(title)
+            person = str(name)
+            loan = [person, book]
+            self.book_loans.append(loan)
+        else:
+            if lib1.check(title) and lib1.check_person(name) and not lib1.check_bookloans(title):
+                book = str(title)
+                person = str(name)
+                loan = [person, book]
+                self.book_loans.append(loan)
 
-        print(bookloans)
+class catalog(Library):
+    def get_all_books():
+        """A method that displays ALL books in the dictionary of the library class."""
+        for book in lib1.book_list:
+            print(book['title'])
 
+if __name__ == "__main__":
+    lib1 = Library()
+    lib1.import_data()
+    lib1.loan("Hisham", 'Pride and Prejudice')
+    lib1.loan("Hisham", 'Things Fall Apart')
+    lib1.loan("Rodin", 'The Sound of the Mountain')
+    lib1.loan("Rodin", 'Berlin Alexanderplatz')
+                                                               #if a person does not exist the loan does not get added,
+    lib1.loan("Maren", 'Berlin Alexanderplatz')                #if a book gets added that doesn't exist neither.
+                                                               #if a book that is already loaned out gets loaned again,
+    lib1.loan("Hisham", "REEEEEE")                             #it does not show up in the bookloans list.
+    print(lib1.book_loans)
 
-if __name__ =="__main__":
-    with open('booksset1.json') as json_file:                                   #To load backup, change json to bookssetbackup.json
-        booklist = json.load(json_file)
-    library = Library(booklist)
+    lib1.check_bookloans('Berlin Alexanderplatz')
+    lib1.check_bookloans("REEEEEE")
 
-    with open('FakeNameSet.csv', 'rt', encoding="utf-8-sig",newline = '') as f: #To load backup, change csv to FakeNameSetbackUp.csv
-        csv_reader = csv.reader(f)
-        customer_list = []
-        for line in csv_reader:
-            customer_list.append(line)
-        customerlist = CustomerAdministration()
-
-    with open('booksset1.json') as json_file:                                   #To load backup, change json to bookssetbackup.json
-        booklist = json.load(json_file)
-        pprint.pprint(booklist)
-        print(customer_list)
-
-
-
-
-
-    #library.get_book_by_author('TestAuthor') #Searching for a book (By author).
-    #library.get_book_by_title('Fairy tales') #Searching for a book (By title).
-    loanbooks.rent_book("Bengt","The Decameron") #Making a book loan for an available book item.
-    #customerlist.add_customer(['21',  'male',  'Dutch',  'Daan',  'Nekeman',  'Straat 1',  '9461 JE',  'Gieten',  'GulserenWilligenburg@teleworm.us',  'Ressoare',  '06-92433659']) #Adding new customers.
-    #customerlist.add_customer(['22',  'male',  'Dutch',  'Danyel',  'Fijten',  'Straat 2',  '9461 JE',  'Gieten',  'GulserenWilligenburg@teleworm.us',  'Ressoare',  '06-92433659'])
-    #customerlist.add_customer(['23',  'male',  'Dutch',  'Wouter',  'van der Schoot',  'Straat 3',  '9461 JE',  'Gieten',  'GulserenWilligenburg@teleworm.us',  'Ressoare',  '06-92433659'])
-    #library.add_book_to_library({"author": "TstAuthor","country": "TestCountry","imageLink": "images/TestImage","language": "TestLanguage","link": "Testlink\n","pages": 1,"title": "No","year": 1836})#Adds a book to library instance #Adding new book.
-    #CustomerAdministration.backup_customers() #backup all people currently in the customer list to a csv file
-    #Library.backup_library(library, booklist) #backups the instance of library to the file bookssetbackup.json included in the assignment.
-    #catalog.get_all_books() # Prints all titles
-    #author1 = authors("Danyel",["how2sD","DanyelBiography"]) #Create new author with name and titles
-    #authors.get_name(author1) #Get author name
-    #authors.get_titles(author1) #Get author titles
+    #lib1.get_book_by_title("The Sound of the Mountain")
+    lib1.add_book_to_library({"author": "TstAuthor","country": "TestCountry","imageLink": "images/TestImage","language": "TestLanguage","link": "Testlink\n","pages": 1,"title": "No","year": 1836})
+    lib1.backup_library(lib1.book_list)
+    lib1.add_customer(['21',  'male',  'Dutch',  'Daan',  'Nekeman',  'Straat 1',  '9461 JE',  'Gieten',  'GulserenWilligenburg@teleworm.us',  'Ressoare',  '06-92433659'])
+    lib1.backup_customers(lib1.customer_list)
+    lib1.backup_loans(lib1.book_loans)
